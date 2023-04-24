3e322a76ffda4c3c38d78374d9c3731373a0737f
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-24 16:28:41 +0530

    Update cosmosdb_failover.ps1

--- cosmosdb_failover.ps1
+++ cosmosdb_failover.ps1
@@ -5,4 +5,4 @@ param(
 [Parameter(Mandatory=$true)][string]$cosmosdb_secondary_region
 )                                                          
 
-Update-AzCosmosDBAccountFailoverPriority -ResourceGroupName $cosmosdb_resource_group_name -Name $cosmosdb_name -FailoverPolicy $cosmosdb_primary_region, $cosmosdb_secondary_region
+Update-AzCosmosDBAccountFailoverPriority -ResourceGroupName $cosmosdb_resource_group_name -Name $cosmosdb_name -FailoverPolicy $cosmosdb_secondary_region, $cosmosdb_primary_region
