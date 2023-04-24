31bd91637381f656a277d929868dfa1d22a541df
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-24 14:20:50 +0530

    Update Link_pe_to_dns_zone.ps1

--- Link_pe_to_dns_zone.ps1
+++ Link_pe_to_dns_zone.ps1
@@ -1,26 +1,44 @@
 param(
-[Parameter(Mandatory=$true)][string]$private_dns_zone_subscription_Id,                   #Enter the primary subscription Id of private DNS zone
-[Parameter(Mandatory=$true)][string]$secondary_private_endpoint_subscription_Id,         #Enter the secondary subscription Id of secondary private endpoint
-[Parameter(Mandatory=$true)][string]$secondary_private_endpoint_resource_group_name,     #Enter the secondary resourcegroup name where the secondary private endpoint is existing           
-[Parameter(Mandatory=$true)][string]$secondary_private_endpoint_name,                   #Enter the secondary private endpoint name
-[Parameter(Mandatory=$true)][string]$cosmosdb_private_dns_zone_name,                    #Enter the name of the existing Private DNS Zone
-[Parameter(Mandatory=$true)][string]$cosmosdb_account_private_dns_zone_group_name,       #Enter the name of the existing Private DNS Zone group
-[Parameter(Mandatory=$true)][string]$private_dns_zone_resource_group_name
+[Parameter(Mandatory=$true)][string]$private_dns_zone_subscription_Id,           #Enter the Subscription Id where the Private dns zone is existing
+[Parameter(Mandatory=$true)][string]$private_dns_zone_resource_group_name,        #Enter the resourcegroup name of the Private dns zone
+[Parameter(Mandatory=$true)][string]$cosmosdb_private_dns_zone_name,               #Enter the name of the private dns zone
+[Parameter(Mandatory=$true)][string]$cosmosdb_private_dns_zone_global_record_name,
+[Parameter(Mandatory=$true)][string]$cosmosdb_private_dns_zone_primary_region_record_name,
+[Parameter(Mandatory=$true)][string]$cosmosdb_private_dns_zone_secondary_region_record_name,             #Enter the name of the recordname to be updated
+[Parameter(Mandatory=$true)][string]$cosmosdb_global_endpoint_primary_region_IP,                   #Enter the new IP to be added
+[Parameter(Mandatory=$true)][string]$cosmosdb_global_endpoint_secondary_region_IP,                   #Enter the old IP to be removed
+[Parameter(Mandatory=$true)][string]$cosmosdb_primary_endpoint_primary_region_IP,
+[Parameter(Mandatory=$true)][string]$cosmosdb_primary_endpoint_secondary_region_IP,
+[Parameter(Mandatory=$true)][string]$cosmosdb_secondary_endpoint_primary_region_IP,
+[Parameter(Mandatory=$true)][string]$cosmosdb_secondary_endpoint_secondary_region_IP
 )
 
 #The below command sets authentication information for cmdlets that run in the current session
 Set-AzContext -Subscription $private_dns_zone_subscription_Id
 
-#The below commands fetches the existing Private DNS Zone information to be linked
-$Zone = Get-AzPrivateDnsZone -ResourceGroupName $private_dns_zone_resource_group_name -Name $cosmosdb_private_dns_zone_name
-$PrivateDnsZoneId = $zone.ResourceId
+#The below commands adds the new IP to the DNS recordset
+$RecordSet_global = Get-AzPrivateDnsRecordSet -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $cosmosdb_private_dns_zone_name -Name $cosmosdb_private_dns_zone_global_record_name -RecordType A
+$RecordSet_primary_region = Get-AzPrivateDnsRecordSet -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $cosmosdb_private_dns_zone_name -Name $cosmosdb_private_dns_zone_primary_region_record_name -RecordType A
+$RecordSet_secondary_region = Get-AzPrivateDnsRecordSet -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $cosmosdb_private_dns_zone_name -Name $cosmosdb_private_dns_zone_secondary_region_record_name -RecordType A
 
-#The below commands creates config for private DNS zone integration
-$config = New-AzPrivateDnsZoneConfig -Name $cosmosdb_private_dns_zone_name -PrivateDnsZoneId $PrivateDnsZoneId
+Add-AzPrivateDnsRecordConfig -RecordSet $RecordSet_global -Ipv4Address $cosmosdb_global_endpoint_secondary_region_IP
+Add-AzPrivateDnsRecordConfig -RecordSet $RecordSet_primary_region -Ipv4Address $cosmosdb_primary_endpoint_secondary_region_IP
+Add-AzPrivateDnsRecordConfig -RecordSet $RecordSet_secondary_region -Ipv4Address $cosmosdb_secondary_endpoint_secondary_region_IP
+ 
+Set-AzPrivateDnsRecordSet -RecordSet $RecordSet_global
+Set-AzPrivateDnsRecordSet -RecordSet $RecordSet_primary_region
+Set-AzPrivateDnsRecordSet -RecordSet $RecordSet_secondary_region
 
-#The below command sets authentication information for cmdlets that run in the current session
-Set-AzContext -Subscription $secondary_private_endpoint_subscription_Id
+#The below commands removes the old IP from the DNS recordset
+#$RecordSet = Get-AzPrivateDnsRecordSet -Name $container_registry_private_dns_zone_record_name -RecordType A -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $container_registry_private_dns_zone_name
+$RecordSet_global = Get-AzPrivateDnsRecordSet -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $cosmosdb_private_dns_zone_name -Name $cosmosdb_private_dns_zone_global_record_name -RecordType A
+$RecordSet_primary_region = Get-AzPrivateDnsRecordSet -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $cosmosdb_private_dns_zone_name -Name $cosmosdb_private_dns_zone_primary_region_record_name -RecordType A
+$RecordSet_secondary_region = Get-AzPrivateDnsRecordSet -ResourceGroupName $private_dns_zone_resource_group_name -ZoneName $cosmosdb_private_dns_zone_name -Name $cosmosdb_private_dns_zone_secondary_region_record_name -RecordType A
 
-#The below command links the secondary private endpoint to the existing Private DNS zone
-Set-AzPrivateDnsZoneGroup -ResourceGroupName $secondary_private_endpoint_resource_group_name -PrivateEndpointName $secondary_private_endpoint_name -Name $private_dns_zone_resource_group_name -PrivateDnsZoneConfig $config  
+Remove-AzPrivateDnsRecordConfig -RecordSet $RecordSet_global -Ipv4Address $cosmosdb_global_endpoint_primary_region_IP
+Remove-AzPrivateDnsRecordConfig -RecordSet $RecordSet_primary_region -Ipv4Address $cosmosdb_primary_endpoint_primary_region_IP
+Remove-AzPrivateDnsRecordConfig -RecordSet $RecordSet_secondary_region -Ipv4Address $cosmosdb_secondary_endpoint_primary_region_IP
 
+Set-AzPrivateDnsRecordSet -RecordSet $RecordSet_global
+Set-AzPrivateDnsRecordSet -RecordSet $RecordSet_primary_region
+Set-AzPrivateDnsRecordSet -RecordSet $RecordSet_secondary_region
